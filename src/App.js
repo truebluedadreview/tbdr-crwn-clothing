f2785c06e615e4fd2990181a70becf23e1d15c82
True Blue <truebluedadreview@gmail.com>
2021-12-27 11:30:47 -0800

    implement code that pulls in our shop data from firestore. Also included our WithSpinner HOC

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import { createStructuredSelector } from 'reselect';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component'; 
+import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selector'
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
-  componentDidMount() { 
-    const {setCurrentUser} = this.props;
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -51,17 +51,17 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
-          <Route 
-            exact 
-            path='/signin' 
-            render={() => 
+          <Route
+            exact
+            path='/signin'
+            render={() =>
               this.props.currentUser ? (
                 <Redirect to='/' />
               ) : (
                 <SignInAndSignUpPage />
-                )
-              }
-            />
+              )
+            }
+          />
         </Switch>
       </div>
     );
@@ -77,7 +77,6 @@ const mapDispatchToProps = dispatch => ({
 });
 
 export default connect(
-  mapStateToProps, 
+  mapStateToProps,
   mapDispatchToProps
-  )
-  (App);
+)(App);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,42 +3,71 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const config = {
-    apiKey: "AIzaSyBWeqzjep0R3mHBrRWE_oUtYBexw4y6VwE",
-    authDomain: "crwn-db-dd295.firebaseapp.com",
-    projectId: "crwn-db-dd295",
-    storageBucket: "crwn-db-dd295.appspot.com",
-    messagingSenderId: "611201931405",
-    appId: "1:611201931405:web:83d70ae8876292eadfaac4",
-    measurementId: "G-9S5LS910EB"
-  };
-
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
-
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-    const snapShot = await userRef.get();
-    
-    if(!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
-
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt, 
-                ...additionalData
-            });
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
-    }
-
-    return userRef;
-  };
+  apiKey: "AIzaSyBWeqzjep0R3mHBrRWE_oUtYBexw4y6VwE",
+  authDomain: "crwn-db-dd295.firebaseapp.com",
+  projectId: "crwn-db-dd295",
+  storageBucket: "crwn-db-dd295.appspot.com",
+  messagingSenderId: "611201931405",
+  appId: "1:611201931405:web:83d70ae8876292eadfaac4",
+  measurementId: "G-9S5LS910EB"
+};
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+if (!userAuth) return;
+
+const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+const snapShot = await userRef.get();
+
+if(!snapShot.exists) {
+  const { displayName, email } = userAuth;
+  const createdAt = new Date();
+  try {
+    await userRef.set({
+      displayName,
+      email,
+      createdAt, 
+      ...additionalData
+    });
+  } catch (error) {
+    console.log('error creating user', error.message);
+  }
+}
+
+return userRef;
+};
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
